4afbd0d96c203e157907dd12d8cd3e20ca3884f9
viktor.landstrom <viktor.landstrom@elev.ga.ntig.se>
2019-02-07 08:37:09 +0100

    ganggang

---
+++ src/testCircle.java
@@ -0,0 +1,19 @@
+//import javax.swing.*;
+
+public class testCircle {
+    public static void main(String[] args){
+        Circle c1 = new Circle(0,0,3);
+        Circle c2 = new Circle( 0, 0,3);
+        System.out.println("Arean på circel 1 är " + c1.circumference() + "\nArean på circel 2 är " + c2.circumference());
+        //double resizeD = Double.parseDouble(JOptionPane.showInputDialog(null,"Resize circle 1","",JOptionPane.PLAIN_MESSAGE));
+        //c1.resize(resizeD);
+        System.out.println("Arean på circel 1 är " + c1.circumference() + "\nArean på circel 2 är " + c2.circumference());
+        if (c1.equals(c2)){
+            System.out.println("lika");
+        } else{
+            System.out.println("olika");
+        }
+
+        System.out.println(c1.toString());
+    }
+}
